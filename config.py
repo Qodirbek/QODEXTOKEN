6231b0674228daec5297eb8e191cc4cb1327e5f2
Qodirbek <qodirbekpubg13@gmail.com>
2025-02-08 14:09:15 +0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "qodex_secret"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
